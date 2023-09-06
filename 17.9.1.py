96a5519638cdcb8a00e44144936f27ba45a7cf18
ununicorn <83374291+ununicorn@users.noreply.github.com>
2023-09-06 22:17:20 +0300

    Add files via upload

---
+++ 17.9.1.py
@@ -0,0 +1,27 @@
+array = [int(x) for x in input("Введите числа от 1 до 999 в любом порядке, через пробел: ").split()]
+def merge_sort(array):
+    if len(array) < 2:
+        return array[:]
+    else:
+        middle = len(array) // 2
+        left = merge_sort(array[:middle])
+        right = merge_sort(array[middle:])
+        return merge(left, right)
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+print(merge_sort(array))
